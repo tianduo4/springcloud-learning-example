b6be7bcdde3c41af8834893c92cc0d363d0fc4c2
tianduo4 <tianduo4@163.com>
2018-09-26 00:04:10 +0800

    增加hystrix、zuul学习

---
+++ service-feign/src/main/java/io/github/tianduo4/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package io.github.tianduo4;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry " + name;
+    }
+}
--- service-ribbon/src/main/java/io/github/tianduo4/HelloService.java
+++ service-ribbon/src/main/java/io/github/tianduo4/HelloService.java
@@ -1,5 +1,6 @@
 package io.github.tianduo4;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
